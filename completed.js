c3e57afabd85faebf2755707f1dc093bb1ac1392
Valentina Tabares <valentinatabares212@gmail.com>
2025-04-11 21:24:20 -0500

    feature(completed):creacion funcionalidad completed_002

---
+++ completed.js
@@ -0,0 +1,18 @@
+function markTaskAsCompleted(taskList, taskId) {
+    let found = false;
+  
+    for (let task of taskList) {
+      if (task.id === taskId) {
+        task.completed = true;
+        console.log(`✅ Task "${task.title}" marked as completed!\n`);
+        found = true;
+        return task;
+      }
+    }
+  
+    if (!found) {
+      console.log('❌ Task not found.\n');
+      return "Task not found";
+    }
+  }
+  
